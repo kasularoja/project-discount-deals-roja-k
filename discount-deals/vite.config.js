2c292435a59420f9cbddbd5362393c24bfb0dfcd
Roja <kasularoja67@gmail.com>
2025-06-02 09:13:44 -0500

    deals

--- discount-deals/vite.config.js
+++ discount-deals/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: 'https://kasuLaroja.github.io/project-discount-deals-roja-k/',
   
 })
